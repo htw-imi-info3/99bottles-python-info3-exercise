ab409741663bb791e410d0f80c072d1c18c52ae2
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-21 14:22:13 +0200

    change case order

--- song/song.py
+++ song/song.py
@@ -1,20 +1,20 @@
 class Song():
     def verse(self, n):
-        if n == 2:
+
+        if n == 0:
             return """\
-2 bottles of mate on the wall, 2 bottles of mate.
-Take one down and pass it around, 1 bottle of mate on the wall.
+No more bottles of mate on the wall, no more bottles of mate.
+Go to the store and buy some more, 99 bottles of mate on the wall.
 """
         if n == 1:
             return """\
 1 bottle of mate on the wall, 1 bottle of mate.
 Take it down and pass it around, no more bottles of mate on the wall.
 """
-
-        if n == 0:
+        if n == 2:
             return """\
-No more bottles of mate on the wall, no more bottles of mate.
-Go to the store and buy some more, 99 bottles of mate on the wall.
+2 bottles of mate on the wall, 2 bottles of mate.
+Take one down and pass it around, 1 bottle of mate on the wall.
 """
 
         result = \
