fda91cc07569650bb40c3b77bc8e6e8fd8f7c15f
Barne Kleinen <barne.kleinen@htw-berlin.de>
2023-06-21 14:01:43 +0200

    step 2: shameless green

--- song/song.py
+++ song/song.py
@@ -5,6 +5,11 @@ class Song():
 85 bottles of mate on the wall, 85 bottles of mate.
 Take one down and pass it around, 84 bottles of mate on the wall.
 """
+        if n == 86:
+            return """\
+86 bottles of mate on the wall, 86 bottles of mate.
+Take one down and pass it around, 85 bottles of mate on the wall.
+"""
 
         return "not implemented"
 
